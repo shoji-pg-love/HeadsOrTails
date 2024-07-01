00a4312baeb32b081243fee905e9721a9dd5324a
unknown <eri618888@gmail.com>
2024-07-01 16:02:51 +0900

    HoT: initial revision

---
+++ HeadsOrTails.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string coin_toss() {
+    return rand() % 2 == 0 ? "Heads" : "Tails";
+}
+
+int main() {
+    srand(time(0));
+    cout << "Tossing a coin..." << endl;
+    vector<string> results;
+    for (int i = 1; i <= 3; ++i) {
+        string result = coin_toss();
+        results.push_back(result);
+        cout << "Round " << i << ": " << result << endl;
+    }
+
+    int h_cnt = count(results.begin(), results.end(), "Heads");
+    int t_cnt = count(results.begin(), results.end(), "Tails");
+    cout << "Heads: " << h_cnt << ", Tails: " << t_cnt << endl;
+
+    return 0;
+}
